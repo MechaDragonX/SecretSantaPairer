b30a7b4d170ca587a645ef3ece1c6972d4d13535
MechaDragonX <sonicraghav@gmail.com>
2019-12-03 09:56:04 -0800

    Make interactive

--- SecretSantaPairer/Program.cs
+++ SecretSantaPairer/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SecretSantaPairer
 {
@@ -11,6 +9,25 @@ namespace SecretSantaPairer
     {
         static void Main(string[] args)
         {
+            Write(Pair(GetNames()));
+            Console.ReadKey();
+        }
+        private static string[] GetNames()
+        {
+            Console.WriteLine("Welcome to the Secret Santa Pairer program!");
+            Console.WriteLine("Please type in the name of each person in the exchange followed by a return. If there are no names, type \"done\"\n");
+            List<string> people = new List<string>();
+            string input = "";
+            while(true)
+            {
+                input = Console.ReadLine();
+                if(input.ToLower() != "done")
+                    people.Add(input);
+                else break;
+            }
+            // Add a blank line just for aesthetics
+            Console.WriteLine();
+            return people.ToArray();
         }
         private static string[] Pair(string[] people)
         {
@@ -18,23 +35,23 @@ namespace SecretSantaPairer
             List<string> santas = people.ToList();
             List<string> santees = people.ToList();
             Random rng = new Random();
-            for (int i = 0; i < people.Length; i++)
+            for(int i = 0; i < people.Length; i++)
             {
                 string santa = santas[rng.Next(0, santas.Count)];
                 santas.Remove(santa);
                 string santee = "";
                 string test;
                 // Make sure a unique santee is generated
-                while (santee == "")
+                while(santee == "")
                 {
                     test = santees[rng.Next(0, santees.Count)];
-                    if (santa != test)
+                    if(santa != test)
                     {
                         santee = test;
                     }
                 }
                 santees.Remove(santee);
-                if (i < people.Length - 1)
+                if(i < people.Length - 1)
                     output[i] = string.Format("{0} is {1}'s santa!", santa, santee);
                 else
                     output[i] = string.Format("{0} is {1}'s santa!", santa, santee);
