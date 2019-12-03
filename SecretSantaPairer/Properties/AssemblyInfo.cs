09afa3e5162434316e17afa8c8f739bf6437fc23
MechaDragonX <sonicraghav@gmail.com>
2019-12-03 13:46:15 -0800

    Add ico file

--- SecretSantaPairer/Properties/AssemblyInfo.cs
+++ SecretSantaPairer/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SecretSantaPairer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Randomly sets pairs for a Secret Santa gift exchange")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MechaDragonX")]
 [assembly: AssemblyProduct("SecretSantaPairer")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("MechaDragonX")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
